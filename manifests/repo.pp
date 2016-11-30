92cab3c5d7eaa846a50b9d243e85c8fdfd513a2f
Joshua Knarr <Joshua.Knarr@LexisNexis.com>
2016-11-30 16:31:12 -0500

    adding gpgcakey, and proxy properties

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -4,14 +4,18 @@
 #
 #
 define yum::repo (
-  $enabled    = 1,
-  $gpg_source = undef,
-  $baseurl    = undef,
-  $descr      = undef,
-  $exclude    = undef,
-  $gpgcheck   = undef,
-  $gpgkey     = undef,
-  $mirrorlist = undef,
+  $enabled        = 1,
+  $gpg_source     = undef,
+  $baseurl        = undef,
+  $descr          = undef,
+  $exclude        = undef,
+  $gpgcakey       = undef,
+  $gpgcheck       = undef,
+  $gpgkey         = undef,
+  $mirrorlist     = undef,
+  $proxy          = undef,
+  $proxy_password = undef, 
+  $proxy_username = undef,
 ) {
 
   if !$baseurl and !$mirrorlist {
@@ -26,13 +30,17 @@ define yum::repo (
   }
 
   yumrepo { $name:
-    enabled    => $enabled,
-    baseurl    => $baseurl,
-    descr      => $descr,
-    exclude    => $exclude,
-    gpgcheck   => $gpgcheck,
-    gpgkey     => $gpgkey,
-    mirrorlist => $mirrorlist,
+    enabled        => $enabled,
+    baseurl        => $baseurl,
+    descr          => $descr,
+    exclude        => $exclude,
+    gpgcakey       => $gpgcakey,
+    gpgcheck       => $gpgcheck,
+    gpgkey         => $gpgkey,
+    mirrorlist     => $mirrorlist,
+    proxy          => $proxy,
+    proxy_password => $proxy_password,
+    proxy_username => $proxy_username,
   }
 
   if $gpg_source and $gpg_source =~ /^puppet:/ {
